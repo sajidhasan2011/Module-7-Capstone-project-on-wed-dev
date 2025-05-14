e156f677903048d05759e51e2742311561d57037
Sajid <afrojaakter874@gmail.com>
2025-05-14 19:15:49 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name, job) {
+    this.name = name;
+    this.job = job;
+  }
+
+  print() {
+    const { name, job } = this;
+    console.log(` ${name},  ${job}`);
+  }
+}
